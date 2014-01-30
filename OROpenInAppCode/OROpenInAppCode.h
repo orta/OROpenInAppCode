a9b82a868595739e70144cfec2d519a729236575
Orta Therox <orta.therox@gmail.com>
2014-01-30 11:53:40 -0500

    initial commit

---
+++ OROpenInAppCode/OROpenInAppCode.h
@@ -0,0 +1,13 @@
+//
+//  OROpenInAppCode.h
+//  OROpenInAppCode
+//
+//  Created by Orta on 30/01/2014.
+//  Copyright (c) 2014 Orta Therox. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface OROpenInAppCode : NSObject
+
+@end
---
+++ OROpenInAppCode/OROpenInAppCode.m
@@ -0,0 +1,99 @@
+//
+//  OROpenInAppCode.m
+//  OROpenInAppCode
+//
+//  Created by Orta on 30/01/2014.
+//    Copyright (c) 2014 Orta Therox. All rights reserved.
+//
+// Totally inspired by https://github.com/inquisitiveSoft/AJKExtendedOpening/blob/master/Classes/AJKExtendedOpening.m
+
+#import "OROpenInAppCode.h"
+
+static OROpenInAppCode *sharedPlugin;
+
+@interface OROpenInAppCode()
+@end
+
+@implementation OROpenInAppCode
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+    static id sharedPlugin = nil;
+    static dispatch_once_t onceToken;
+    NSString *currentApplicationName = [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
+    if ([currentApplicationName isEqual:@"Xcode"]) {
+        dispatch_once(&onceToken, ^{
+            sharedPlugin = [[self alloc] initWithBundle:plugin];
+        });
+    }
+}
+
+- (id)initWithBundle:(NSBundle *)plugin
+{
+    if (self = [super init])
+    if(!self) return nil;
+
+    [self setup];
+
+    return self;
+}
+
+
+- (void)setup
+{
+
+    // Add menu bar items for the 'Show Project in Finder' and 'Open Project in Terminal' actions
+    NSMenu *fileMenu = [[[NSApp mainMenu] itemWithTitle:@"File"] submenu];
+    NSInteger desiredMenuItemIndex = [fileMenu indexOfItemWithTitle:@"Open with External Editor"];
+
+    if(fileMenu && (desiredMenuItemIndex >= 0)) {
+
+        NSMenuItem *openWithExternalEditorMenuItem = [[NSMenuItem alloc] initWithTitle:@"Open Project in AppCode" action:@selector(openInAppCode:) keyEquivalent:@"a"];
+        [openWithExternalEditorMenuItem setTarget:self];
+        [openWithExternalEditorMenuItem setKeyEquivalentModifierMask:NSCommandKeyMask | NSControlKeyMask];
+        [fileMenu insertItem:openWithExternalEditorMenuItem atIndex:desiredMenuItemIndex];
+
+
+    } else if([NSApp mainMenu]) {
+        NSLog(@"OROpenInAppCode Xcode plugin: Couldn't find an 'Open with External Editor' item in the File menu");
+    }
+}
+
+- (void)openInAppCode:(id)sender
+{
+    NSURL *currentFileURL = [self currentProjectURL];
+    if(currentFileURL) {
+        [[NSWorkspace sharedWorkspace] openURLs:@[currentFileURL]
+                        withAppBundleIdentifier:@"com.jetbrains.AppCode"
+                                        options:0
+                 additionalEventParamDescriptor:nil
+                              launchIdentifiers:nil];
+    }
+}
+
+- (NSURL *)currentProjectURL
+{
+    for (NSDocument *document in [NSApp orderedDocuments]) {
+        @try {
+            //        _workspace(IDEWorkspace) -> representingFilePath(DVTFilePath) -> relativePathOnVolume(NSString)
+            NSURL *workspaceDirectoryURL = [[[document valueForKeyPath:@"_workspace.representingFilePath.fileURL"] URLByDeletingLastPathComponent] filePathURL];
+
+            if(workspaceDirectoryURL) {
+                return workspaceDirectoryURL;
+            }
+        }
+
+        @catch (NSException *exception) {
+            NSLog(@"OROpenInAppCode Xcode plugin: Raised an exception while asking for the documents '_workspace.representingFilePath.relativePathOnVolume' key path: %@", exception);
+        }
+    }
+
+    return nil;
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+@end
